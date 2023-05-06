30efc24aae3fa1ac849dc9cd923272881923930a
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-05-06 18:14:13 +0200

    Remove unused code

--- js/main.js
+++ js/main.js
@@ -5,24 +5,8 @@ function load(currentLng, tag){
     translate.process(); 
 }
 
-function refreshCSS() {
-    let links = document.getElementsByTagName('link');
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].getAttribute('rel') == 'stylesheet') {
-            let href = links[i].getAttribute('href')
-                                    .split('?')[0];
-              
-            let newHref = href + '?version=' 
-                        + new Date().getMilliseconds();
-              
-            links[i].setAttribute('href', newHref);
-        }
-    }
-}
-
 // Parse language parameter from URL (default to browser language if not specified)
 const urlParams = new URLSearchParams(window.location.search);
 const userLang = urlParams.get("lang") || navigator.language.slice(0, 2);
 
 load(userLang, 'data-tag');
-refreshCSS();
