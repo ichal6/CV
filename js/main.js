4df39808d771b2ae2b12a5263f268afd4fba2be8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-05-06 22:59:18 +0200

    Repair typo

--- js/main.js
+++ js/main.js
@@ -5,8 +5,22 @@ function load(currentLng, tag){
     translate.process(); 
 }
 
+function lastSingleLetterToNewLine(el){
+    let result;
+    el.forEach((element, i)=>{
+       if(!element.innerHTML.match(/[{}]|<script|^\n$/gi)){
+        result = element.innerHTML.replace(/ (.) /gi, " "+'\$1'+"&nbsp;");
+       }
+       element.innerHTML = result;
+    });
+}
+
 // Parse language parameter from URL (default to browser language if not specified)
 const urlParams = new URLSearchParams(window.location.search);
 const userLang = urlParams.get("lang") || navigator.language.slice(0, 2);
 
 load(userLang, 'data-tag');
+
+let el = document.querySelectorAll("h1, h2, h3, h4, h5, p, span, .text");
+
+lastSingleLetterToNewLine(el);
